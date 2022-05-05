0308707b850442519100ba9fd27aba0261846265
Student 249860 <lemihdesign@gmail.com>
2022-05-05 15:43:00 +0200

    Simple form validation

--- src/components/Home/Form/Form.js
+++ src/components/Home/Form/Form.js
@@ -1,13 +1,40 @@
-import { useSelector, useDispatch } from "react-redux";
+import { useDispatch } from "react-redux";
+import useInput from "../../../hooks/use-input";
 import { formActions } from "../../../store/form-slice";
 
 const Form = () => {
-  const nameValue = useSelector((state) => state.form.name);
   const dispatch = useDispatch();
+
+  const {
+    value: nameInputValue,
+    hasError: nameInputHasError,
+    isValid: nameInputIsValid,
+    valueChangeHandler: nameInputChangeHandler,
+    blurChangeHandler: nameBlurChangeHandler,
+    reset: nameInputReset,
+  } = useInput(
+    (value) =>
+      value.trim() !== "" && value.includes(" ") && value.trim().length > 4
+  );
+
+  let formIsValid = false;
+
+  if (nameInputIsValid) {
+    formIsValid = true;
+  }
+
+  const nameInputClasses = nameInputHasError ? "invalid" : "";
 
   const formSubmitHandler = (e) => {
     e.preventDefault();
+
+    if (!formIsValid) {
+      return;
+    }
+
     dispatch(formActions.changeName(e.target.value));
+
+    nameInputReset();
   };
 
   return (
@@ -15,8 +42,28 @@ const Form = () => {
       <h2>let us optimize your home affairs</h2>
       <form onSubmit={formSubmitHandler}>
         <label htmlFor="name">Your name and surname</label>
-        <input type="text" id="name" value={nameValue} />
-        <button type="submit">Confirm</button>
+        <input
+          className={nameInputClasses}
+          type="text"
+          id="name"
+          value={nameInputValue}
+          onChange={nameInputChangeHandler}
+          onBlur={nameBlurChangeHandler}
+        />
+        {nameInputHasError && (
+          <p
+            style={{
+              color: "crimson",
+              fontWeight: "600",
+              fontSize: "13px",
+            }}
+          >
+            Please enter a valid first name.
+          </p>
+        )}
+        <button disabled={!formIsValid} type="submit">
+          Confirm
+        </button>
       </form>
     </div>
   );
---
+++ src/hooks/use-input.js
@@ -0,0 +1,52 @@
+import { useReducer } from "react";
+
+const initialInputState = {
+  value: "",
+  isTouched: false,
+};
+
+const initialStateReducer = (state, action) => {
+  if (action.type === "INPUT") {
+    return { value: action.value, isTouched: state.isTouched };
+  }
+  if (action.type === "BLUR") {
+    return { value: state.value, isTouched: true };
+  }
+  if (action.type === "RESET") {
+    return { value: "", isTouched: false };
+  }
+  return initialInputState;
+};
+
+const useInput = (validateFunction) => {
+  const [inputState, dispatch] = useReducer(
+    initialStateReducer,
+    initialInputState
+  );
+
+  const valueChangeHandler = (e) => {
+    dispatch({ type: "INPUT", value: e.target.value });
+  };
+
+  const blurChangeHandler = () => {
+    dispatch({ type: "BLUR" });
+  };
+
+  const reset = () => {
+    dispatch({ type: "RESET" });
+  };
+
+  const isEnteredInputValid = validateFunction(inputState.value);
+  const hasError = !isEnteredInputValid && inputState.isTouched;
+
+  return {
+    value: inputState.value,
+    hasError,
+    isValid: isEnteredInputValid,
+    valueChangeHandler,
+    blurChangeHandler,
+    reset,
+  };
+};
+
+export default useInput;
