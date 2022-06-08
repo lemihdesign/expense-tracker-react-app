789cec3a04268b6665e74058e42b26ce13591f0c
Student 249860 <lemihdesign@gmail.com>
2022-06-08 17:12:49 +0200

    Some default styles for tasks boxes.

--- src/components/To-Do-List/To-Do-ItemsList/ToDoItem.js
+++ src/components/To-Do-List/To-Do-ItemsList/ToDoItem.js
@@ -1,14 +1,23 @@
+import classes from "./ToDoItem.module.css";
+
 const ToDoItem = (props) => {
   const { name, description, steps } = props;
 
-  const stepsList = steps.map((step) => <li key={step.id}>{step.text}</li>);
+  const stepsList = steps.map((step) => (
+    <li key={step.id} className={classes["task-list-item"]}>
+      <i className="fa-solid fa-check"></i>
+      {step.text}
+    </li>
+  ));
 
   console.log(steps);
   return (
-    <div>
-      <p>{name}</p>
-      <p>{description}</p>
-      <ul>{stepsList}</ul>
+    <div className={classes["task-box"]}>
+      <p className={classes["task-name"]}>{name}</p>
+      <p className={classes["task-description"]}>
+        Główny cel zadania: {description}
+      </p>
+      <ul className={classes["task-list"]}>{stepsList}</ul>
     </div>
   );
 };
